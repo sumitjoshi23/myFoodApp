5ca237b6aea72d265dfefdbeb6f66600d183ad3c
Sumit <joshiz.sumit@gmail.com>
2023-03-23 03:23:10 +0530

    Minor code refactoring

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,23 +4,23 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <>
-      <footer className="w-full bg-gray-800 text-white">
+      <footer className="w-full bg-[#ef8a68]">
         <div className="max-w-7xl mx-auto py-6 px-4">
           <div className="flex justify-between">
             <div>
-              <p className="text-center text-gray-300">
+              <p className="text-center">
                 &copy; J Foods and Co. Pinched with ❤️. All rights reserved.
               </p>
             </div>
             <div className="flex justify-center ">
               <nav className="flex space-x-4 mr-8">
-                <Link to="/offers" className="text-gray-300 hover:text-white">
+                <Link to="/offers" className="hover:text-white">
                   Offers
                 </Link>
-                <Link to="/about" className="text-gray-300 hover:text-white">
+                <Link to="/about" className="hover:text-white">
                   About
                 </Link>
-                <Link to="/contact" className="text-gray-300 hover:text-white">
+                <Link to="/contact" className="hover:text-white">
                   Contact Us
                 </Link>
               </nav>
