ca13dbaa7ba5a52fff8df96c10bf51ef3ccd10f4
sumit.joshi@qburst.com <sumit.joshi@qburst.com>
2025-04-24 14:58:59 +0530

    Added alignmet fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import ScrollToTop from "./components/ScrollToTop";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import { Outlet } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store";
-import FixedSideBars from "./components/FixedSideBars";
+import ScrollToTop from './components/ScrollToTop';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Outlet } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import FixedSideBars from './components/FixedSideBars';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <ScrollToTop />
       <div className="flex flex-col">
         <Header />
-        <div className="container py-5 mx-auto px-36 min-h-screen">
+        <div className="container py-5 mx-auto px-24 min-h-screen">
           <Outlet />
         </div>
         <Footer />
